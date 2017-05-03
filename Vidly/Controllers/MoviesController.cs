9eb77cabf1a7d7a28db039b7a9840b80216baa91
anatolylevchenko <kypiwindy@xaker.ru>
2017-05-03 13:19:21 +0300

    Init

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        
+        public ActionResult Random()
+        {
+            Movie movie=new Movie() {Name = "Shrek"};
+
+            return View(movie);
+        }
+        [Route("movies/release/{year}/{month:regex(\\d2):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+
+            return Content(year + "/" + month);
+        }
+    }
+}
