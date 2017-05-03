4b6b6a80f248039321a999d8b4db3278e7682676
anatolylevchenko <kypiwindy@xaker.ru>
2017-05-03 15:43:27 +0300

    Add Membership Type

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -2,7 +2,10 @@
 {
     public class Customer
     {
+        public bool IsSubscribedToNewsletter { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
